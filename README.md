05db9562305bd98acc5ab128b047f82940131401
TheVoidsteppers <2300769284@qq.com>
2024-11-06 20:06:20 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hammer-dressing
-vscode 拓展
+
+This extension provide some development tools to Visual Studio Code.
+
+## Usage
+
+see the `Feature Contributions` tab
+
+## Installation
+
+You can install this extension through the [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=TheVoidsteppers.hammer-dressing)
+
+1.  Open **Extensions** sidebar panel in VS Code. `View → Extensions`
+
+2.  Search for `hammerDressing`
+
+3.  Click **Install** to install the extension
---
+++ dist/extension.js
@@ -0,0 +1,39 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */
+/***/ (function(__unused_webpack_module, exports) {
+
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.activate = void 0;
+function activate(context) {
+    return __awaiter(this, void 0, void 0, function* () {
+    });
+}
+exports.activate = activate;
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__[0](0, __webpack_exports__);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
